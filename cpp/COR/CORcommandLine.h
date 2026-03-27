5fb45a712060ddec78f6e8227a9249da9039e086
Eliot Muir <eliot.muir@interfaceware.com>
2026-03-27 12:32:23 -0500

    Got rid of verbose comments

--- cpp/COR/CORcommandLine.h
+++ cpp/COR/CORcommandLine.h
@@ -6,37 +6,10 @@
 // 
 // Description
 //
-// This is utility class which makes it easy to get command line
-// arguments.  
+// Utility class for parsing command line flags and positional parameters.
 //
-// The command line has the following general form:
-//
-// ApplicationName [--flag1 [argument1]] [--flag2 [argument2]] ... [--flagN [argumentN]]
-//    [extraArgument1] [extraArgument2] ... [extraArgumentM]
-//
-// where the square brackets denote that the value is optional.  Flags are denoted by
-// two dashes '--', and at present a flag may have zero or one arguments following it.
-// A string in the command line which is not a flag, and which is not the argument of
-// a flag, is classified as an 'extra argument'.  All such extra arguments should appear
-// in the command line after all the flags, as shown.
-//
-// Best usage: 
-//
-// For each flag which your parser handles, use addFlagWithoutArgument(FlagName, Description)
-// or addFlagWithArgument(FlagName, ArgumentName, Description) to register the flag with
-// the parser.  Then, for each registered flag, have a conditional:
-// if (LineParser.isFlagPresent("FlagName"))
-// {
-//    implementation details
-// } 
-// (Avoid using flagPresent to implicitly register flags without arguments with the parser, since there
-// is no equivalent method for flags with arguments, and it leads to code that is more confusing than
-// it needs to be.)  
-
-// The interface for the CORcommandLine class also refers to 'parameters'.  Parameters are
-// arguments which are not associated with a flag.  That is, they look just like 'extra
-// arguments' on the command line, but they can have a description associated with them,
-// and they can be required or optional.  
+// Register expected flags first, then call parseArgs(), then query the
+// parsed flags and extra arguments.
 //
 // Author: Eliot Muir
 //---------------------------------------------------------------------------
@@ -51,69 +24,48 @@ public:
    CORcommandLine();
    ~CORcommandLine();
 
-   // Parses the command line.  It registers the flags that are present, and builds
-   // a vector of 'extra arguments'.
-   // This method will return false it if fails - it will also
-   // output an error message to stderr - the user should check the return
-   // value of this class.
+   // Parse flags and positional arguments.
    void parseArgs(int argc, const char** ppArg);
    
-   // returns the program name - argv[0] - which was stored upon parsing above
+   // Return argv[0].
    const CORstring& programName() const;
 
-   // Returns the number of extra arguments found 
+   // Number of positional arguments.
    int countOfExtraArgument() const;
 
-   // Retrieves the value of an extra argument
+   // Positional argument by index.
    const CORstring& extraArgument(int ArgumentIndex) const;
 
-   // Retrieves the value of an extra argument
+   // Positional argument by declared parameter name.
    const CORstring& extraArgument(const CORstring& Parameter) const;
 
-   // Is a flag present? - true/false 
+   // True if a registered flag was present.
    bool isFlagPresent(const CORstring& FlagName) const;
 
-   // Registers a flag without any arguments.  The Description is used
-   // by showUsage().
+   // Register a flag with no argument.
    void addFlagWithoutArgument(const CORstring& FlagName, const CORstring& Description);
 
-   // Registers a flag which has an argument. 
-   // There seems to be some confusion in the libraries about the use
-   // of this method.  ArgumentName is not a substitute for a flag description,
-   // though it has sometimes been erroneously used that way.  Instead, 
-   // ArgumentName should give the form of the argument.  A description can
-   // - and should - be added using flagPresent(FlagName, Description) or, better
-   // yet, use the method below which takes 3 parameters.  Both
-   // the ArgumentName and the Description are used by showUsage().
-   //
-   // For instance, suppose we have a flag '--mailto' which, when present, means
-   // that the output of the command line utility is mailed to a particular
-   // email address.  In this case FlagName = "mailto", ArgumentName might be
-   // something like "user@host", and Description might be "Mail output to 
-   // specified email address."
+   // Register a flag with an argument.
+   // ArgumentName is a usage label such as "<file>".
    void addFlagWithArgument(const CORstring& FlagName, const CORstring& ArgumentName);
 
-   // Register a flag which has an argument, and provide a usage description.
+   // Register a flag with an argument and usage description.
    void addFlagWithArgument(const CORstring& FlagName,
                             const CORstring& ArgumentName,
                             const CORstring& Description);
 
-   //get argument for a flag. returns "" if flag doesn't have arguments
+   // Retrieve a flag argument. Returns "" if the flag has no argument.
    void flagArgument(const CORstring& FlagName, CORstring& Argument) const;
 
    const CORstring& flagArgument(const CORstring& FlagName) const;
 
-   // This command will display the list of flags that the class knows about
+   // Print usage for registered flags and parameters.
    void showUsage(CORostream& OutStream) const;
 
-   // hides a flag from the showUsage and --kissbook call
+   // Hide a flag from showUsage().
    void hideFlag(const CORstring& FlagName);
    
-   // If an error was encountered while parsing the arguments, return true.
-   // Also, will write a summary of the error to ErrorStream.
-   // Possible errors are:
-   //  * An undefined flag was present
-   //  * A flag was used more than once
+   // Return true and write an error summary if parsing found a problem.
    bool parsingErrorsPresent(CORostream& ErrorStream) const;
 
    void addExtraParamDescription(const CORstring& Parameter, const CORstring& Description);
